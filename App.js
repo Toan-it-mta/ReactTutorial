7a345c39ff2956b28fd9e87091c19032880e4ca4
Nguyễn Phúc Toàn <toannp@aiacademy.edu.vn>
2022-06-10 16:10:51 +0700

    Hoan thanh Tutorial4

--- App.js
+++ App.js
@@ -5,62 +5,95 @@ import {
   Button,
   View,
   ScrollView,
-  RefreshControl
+  FlatList,
+  RefreshControl,
+  SectionList,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const [name, SetName] = useState('My name is: Nguyen Phuc Toan')
+  const [number, SetNumber] = useState(2)
   const [current, SetCurrent] = useState(true)
   const [Items, SetItems] = useState([
-    { key: 1, item: 'item 1' },
-    { key: 2, item: 'item 2' },
-    { key: 3, item: 'item 3' },
-    { key: 4, item: 'item 4' },
-    { key: 5, item: 'item 5' },
-    { key: 6, item: 'item 6' },
-    { key: 7, item: 'item 7' },
-    { key: 8, item: 'item 8' },
-    { key: 9, item: 'item 9' },
-    { key: 10, item: 'item 10' },
-    { key: 11, item: 'item 11' },
+    { word: 'item 1' },
+    { word: 'item 2' },
+    { word: 'item 3' },
+    { word: 'item 4' },
+    { word: 'item 5' },
+    { word: 'item 6' },
+    { word: 'item 7' },
+    { word: 'item 8' },
+    { word: 'item 9' },
+    { word: 'item 10' },
+  ])
+
+  const [Sessions, SetSessions] = useState([
+    {
+      title: 'Section 1',
+      data: ['1-1', '1-2', '1-3'],
+    },
   ])
   const [Refesh, SetRefesh] = useState(false)
 
-  const onPress = () => {
-    if (current) {
-      SetName('Tôi tên là Nguyễn Phúc Toàn')
-      SetCurrent(!current)
-    } else {
-      SetName('My name is: Nguyen Phuc Toan')
-      SetCurrent(!current)
-    }
-  }
-
   const onRefesh = () => {
     SetRefesh(true)
-    SetItems([...Items, { key: 69, item: 'item 69' }])
+    SetItems([...Items, { word: 'item 69' }])
     SetRefesh(false)
   }
 
+  const onSectionListRefresh = () => {
+    SetRefesh(true)
+    SetSessions([...Sessions, {
+      title: 'Section ' + number.toString(),
+      data: [number.toString() + '-1', number.toString() + '-2', number.toString() + '-3'],
+    }]
+    )
+    SetNumber(number + 1)
+    SetRefesh(false)
+  }
+
+
   return (
     <View style={styles.body}>
-      <ScrollView horizontal={false} refreshControl={
-        <RefreshControl
-          refreshing={Refesh}
-          onRefresh={onRefesh}
-          colors={['#546318','#ff00ff']}
-        />
-      }>
-        {Items.map((obj) => {
-          return <View style={styles.item} key={obj.key}>
-            <Text style={styles.text}>{obj.item}</Text>
+      <SectionList
+        keyExtractor={(item, index) => index.toString()}
+        sections={Sessions}
+        renderItem={({ item }) => {
+          return <View style={styles.item}>
+            <Text style={styles.text}>{item}</Text>
           </View>
-        })}
-      </ScrollView>
+        }}
+        renderSectionHeader={({ section }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{section.title}</Text>
+          </View>
+        )
+        }
+        refreshControl={
+          <RefreshControl
+            refreshing={Refesh}
+            onRefresh={onSectionListRefresh}
+            colors={['#ff00ff']}
+          ></RefreshControl>
+        }>
+
+      </SectionList>
+
+      {/* <FlatList data={Items}
+        horizontal={true}
+        keyExtractor={(item, index) => index.toString()}
+        // numColumns = {2}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{item.word}</Text>
+          </View>
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={Refesh}
+            onRefresh={onRefesh}
+            colors={['#ff00ff']} />
+        }></FlatList> */}
     </View>
   );
 };
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ReactTutorial
 Thực hiện các hướng dẫn về React Native
 # Tutorial1
-Thực hiện các thực hành List, ScrollView and Refresh Control
+Thực hiện các thực hành FlatList và SectionList
