f0a33ff6b8d2bbe912095900eb2a9d32dfc8e24b
Nguyễn Phúc Toàn <toannp@aiacademy.edu.vn>
2022-06-10 14:22:31 +0700

    Hoàn thành Tutorial3

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
   Button,
   View,
+  ScrollView,
+  RefreshControl
 } from 'react-native';
 
 import {
@@ -11,37 +13,74 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const [name,SetName] = useState('My name is: Nguyen Phuc Toan')
+  const [name, SetName] = useState('My name is: Nguyen Phuc Toan')
   const [current, SetCurrent] = useState(true)
+  const [Items, SetItems] = useState([
+    { key: 1, item: 'item 1' },
+    { key: 2, item: 'item 2' },
+    { key: 3, item: 'item 3' },
+    { key: 4, item: 'item 4' },
+    { key: 5, item: 'item 5' },
+    { key: 6, item: 'item 6' },
+    { key: 7, item: 'item 7' },
+    { key: 8, item: 'item 8' },
+    { key: 9, item: 'item 9' },
+    { key: 10, item: 'item 10' },
+    { key: 11, item: 'item 11' },
+  ])
+  const [Refesh, SetRefesh] = useState(false)
 
-  const onPress = ()=>{
-    if (current){
+  const onPress = () => {
+    if (current) {
       SetName('Tôi tên là Nguyễn Phúc Toàn')
       SetCurrent(!current)
-    }else{
+    } else {
       SetName('My name is: Nguyen Phuc Toan')
       SetCurrent(!current)
     }
   }
 
+  const onRefesh = () => {
+    SetRefesh(true)
+    SetItems([...Items, { key: 69, item: 'item 69' }])
+    SetRefesh(false)
+  }
+
   return (
-   <View style={styles.body}>
-    <Text style={styles.text}>{name}</Text>
-    <Button title='Change State' onPress={onPress}></Button>
-   </View>
+    <View style={styles.body}>
+      <ScrollView horizontal={false} refreshControl={
+        <RefreshControl
+          refreshing={Refesh}
+          onRefresh={onRefesh}
+          colors={['#546318','#ff00ff']}
+        />
+      }>
+        {Items.map((obj) => {
+          return <View style={styles.item} key={obj.key}>
+            <Text style={styles.text}>{obj.item}</Text>
+          </View>
+        })}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  body:{
-    flex:1,
-    justifyContent:'center',
-    alignItems: 'center',
+  body: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'stretch',
   },
-  text:{
-    color:'#ff9912',
-    fontSize:23,
+  text: {
+    color: '#ffffff',
+    fontSize: 23,
     margin: 20
+  },
+  item: {
+    backgroundColor: '#546318',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10
   }
 });
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ReactTutorial
 Thực hiện các hướng dẫn về React Native
 # Tutorial1
-Thực hiện các thực hành State
+Thực hiện các thực hành List, ScrollView and Refresh Control
