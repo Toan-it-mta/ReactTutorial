44f3804dfcaa9ffd988baf9600de7cfa734f030b
Nguyễn Phúc Toàn <toannp@aiacademy.edu.vn>
2022-06-15 14:10:06 +0700

    Hoan thanh Tutorial6

--- App.js
+++ App.js
@@ -2,32 +2,49 @@ import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
-  Button,
   View,
   ScrollView,
   FlatList,
   RefreshControl,
   SectionList,
-  TextInput
+  TextInput,
+  Button,
+  NativeEventEmitter,
+  Pressable
 } from 'react-native';
 
 
 const App = () => {
-  const [name,SetName] = useState('')
+  const [name, SetName] = useState('')
+  const [submitted, SetSubmitted] = useState(false)
+  const onPressHandler = () => {
+    SetSubmitted(!submitted)
+  }
 
   return (
     <View style={styles.body}>
       <Text style={styles.text}>
-        Tên của bạn là:{name}
+        Vui lòng nập tên của bạn
       </Text>
       <TextInput style={styles.input}
-      multiline
-      placeholder="Vui lập nhập tên của bạn"
-      placeholderTextColor={'#555'}
-      onChangeText={(value)=>SetName(value)}
-      keyboardType='default'
-      // maxLength={10}
-      secureTextEntry></TextInput>
+        multiline
+        placeholder="Vui lập nhập tên của bạn"
+        placeholderTextColor={'#555'}
+        onChangeText={(value) => SetName(value)}
+        keyboardType='default'
+        secureTextEntry>
+      </TextInput>
+      {/* <Button title={submitted ? 'Clear' : 'Submit'}
+        onPress={onPressHandler}></Button> */}
+      <Pressable
+      onPress={onPressHandler}
+      style={styles.button}>
+        <Text style={styles.text}>
+          {submitted ? 'Clear' : 'Submit'}
+        </Text>
+      </Pressable>
+      {submitted ? <Text style={styles.text}>Tên bạn đăng ký là {name}</Text> : null}
+
     </View>
   )
 };
@@ -41,22 +58,29 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#000000',
-    fontSize: 23,
-    margin: 20,
+    fontSize: 19,
   },
   item: {
     backgroundColor: '#546318',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 10
   },
-  input:{
+  input: {
     width: 300,
     borderColor: '#555',
     borderWidth: 1,
     color: '#000000',
     borderRadius: 5,
     fontSize: 15,
+    margin: 12
+  },
+  button:{
+    backgroundColor: '#00ff00',
+    height:60,
+    width:150,
+    alignItems:'center',
+    justifyContent: 'center',
+    borderRadius:10
   }
 });
 
